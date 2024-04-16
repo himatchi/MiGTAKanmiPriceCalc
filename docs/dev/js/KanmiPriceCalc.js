52d5fb1bec785e786ddd5315aa87a52e7623f62a
himatch <deburisu@gmail.com>
2024-04-16 23:56:36 +0900

    devコミット スマホ対応調整

--- docs/dev/js/KanmiPriceCalc.js
+++ docs/dev/js/KanmiPriceCalc.js
@@ -288,18 +288,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
   loaded = true;
 });
-
-document.addEventListener('touchstart', event => {
-  if (event.touches.length > 1) {
-    event.preventDefault();
-  }
-}, true);
-
-let lastTouch = 0;
-document.addEventListener('touchend', event => {
-  const now = window.performance.now();
-  if (now - lastTouch <= 500) {
-    event.preventDefault();
-  }
-  lastTouch = now;
-}, true);
